e54410413647b543ac73f53fa5deb4acd659f226
Nate Murray <nate@natemurray.com>
2011-06-15 10:36:13 -0700

    started basic tokenization for analysis

--- src/clucy/analysis.clj
+++ src/clucy/analysis.clj
@@ -1,9 +1,23 @@
 
 (ns clucy.analysis
-  (:use [clojure.contrib.string :only [as-str]])
+  (:use [clojure.contrib.string :only [as-str]]
+        [clucy.util])
   (:require [clojure.java.io :as io]
-            [clucy.imports :as imp])
-  (:import [java.io StringReader]))
+            [clucy.imports :as imp]))
 
 (imp/import-analysis)
 
+(defn tokenizer [type input & tokenizer-args]
+  (let [klass (condp = type
+                  :ngram NGramTokenizer)
+        reader (make-reader input)]
+  (clojure.lang.Reflector/invokeConstructor 
+   klass (to-array (concat [reader] tokenizer-args)))))
+
+
+(comment
+
+  (let [tok (tokenizer :ngram "my dog has fleas" 1 1)]
+    )
+
+  )
