1dc6a2b2f8444db380894ad90b2ce6ff74106082
Nate Murray <nate@natemurray.com>
2011-06-15 14:56:18 -0700

    added ngram tokenizer test

--- test/clucy/test/analysis.clj
+++ test/clucy/test/analysis.clj
@@ -4,8 +4,16 @@
         clojure.test))
 
 (deftest tokenizers
+
   (testing "standard tokenizer"
     (let [tok (tokenizer :standard "The dog said, \"hello you.\"")]
       (is (= (token-stream-seq tok) 
-             '("The" "dog" "said" "hello" "you"))))))
+             '("The" "dog" "said" "hello" "you")))))
+
+  (testing "ngram tokenizer"
+    (let [tok (tokenizer :ngram "abc def" 2 2)]
+      (is (= (token-stream-seq tok) 
+             '("ab" "bc" "c " " d" "de" "ef")))))
+
+  )
 
